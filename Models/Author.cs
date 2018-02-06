13e1ee9551a1433ab7cc6d80ff8ec92ce5eda642
zhangshiyao1995 <zhangshiyao1995@users.noreply.github.com>
2018-02-05 20:05:42 -0500

    Adding models to views in master

---
+++ Models/Author.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Fisher.Bookstore.Models
+{
+
+    public class Author
+    {
+
+        public int AuthorId { get; set; }
+
+        public string Name { get; set; }
+        
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Fisher.Bookstore.Models
+{
+
+    public class Book
+    {
+
+        public int BookId { get; set; }
+
+        public string Title { get; set; }
+
+        public Author Author { get; set: }
+
+        public void SellBook()
+        {
+            //code to sell book goes here
+        }
+        
+    }
+}
